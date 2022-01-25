5184c3a5386f10caa466f5f69a53007ec93f6bb5
Bakazova Aida <solnyshko669@gmail.com>
2022-01-26 00:09:45 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+//task1
+// let num = 15
+// console.log(num)
+
+//task2
+// let a = 10
+// let b = 2
+// let c = 5
+// console.log(a + b + c)
+
+//task3
+// let num = 100
+// console.log(num * 2)
+
+//task4
+// let num = 67
+// console.log(num % 7)
+
+//task5
+// let a = 10
+// let b = 2
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a*b)
+
+//task6
+// let c=15
+// let d=2
+// let result= c+d
+// console.log(result)
+
+//task7
+// let a=30
+// let b=15
+// let c=a+b
+// console.log(c%4)
+
+//task8
+// let a=17
+// let b=10
+// let c=a-b
+// let d=12
+// let result=c+d
+// console.log(result)
+
+//task9
+let a = ''
+
